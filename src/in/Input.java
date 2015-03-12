2bff6839561d1e58647d47f170b2e84097ef3cf0
Duane Bekaert <duane.bekaert@gmail.com>
2015-03-12 19:52:53 +0100

    Added the base Input class

--- src/in/Input.java
+++ src/in/Input.java
@@ -1,7 +1,63 @@
 package in;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by hugo on 12/03/15.
  */
+
 public class Input {
+
+    public class Server {
+        public int index;
+        public int slot;
+        public int capacity;
+    }
+
+    public class UnavailableSlot {
+        public int row;
+        public int slot;
+    }
+
+    protected long rows = 0;
+    protected long slots = 0;
+    protected long pools = 0;
+
+    protected HashMap<Integer, Server> servers;
+    protected HashMap<Integer, UnavailableSlot> unavailableSlots;
+
+    public Input(String file) {
+        servers = new HashMap<Integer, Server>();
+        unavailableSlots = new HashMap<Integer, UnavailableSlot>();
+    }
+
+    public long getRows() {
+        return rows;
+    }
+
+    public long getSlots() {
+        return slots;
+    }
+
+    public long getPoolsCount() {
+        return pools;
+    }
+
+    public HashMap<Integer, Server> getServers() {
+        return servers;
+    }
+
+    public long getServersCount() {
+        return servers.size();
+    }
+
+    public HashMap<Integer, UnavailableSlot> getUnavailableSlots() {
+        return unavailableSlots;
+    }
+
+    public long getUnavailableSlotsCount() {
+        return unavailableSlots.size();
+    }
+
 }
